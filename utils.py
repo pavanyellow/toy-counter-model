30da938ba502b1f7d133faec60a87c7fa5a3082e
Pavan Katta <pavanyellow@gmail.com>
2024-02-06 10:27:35 +0530

    correct hyperparams

---
+++ utils.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from dataclasses import dataclass
+
+import random
+import matplotlib.pyplot as plt
+import time
+import numpy as np
+import plotly.express as px
+
+import transformer_lens.utils as utils
+
+DEVICE = torch.device('cpu')
+
+
+
+
+
+
+
+
+
+def imshow(tensor, **kwargs):
+    px.imshow(
+        utils.to_numpy(tensor),
+        color_continuous_midpoint=0.0,
+        color_continuous_scale="RdBu",
+        **kwargs,
+    ).show()
+
+
+def line(tensor, **kwargs):
+    px.line(
+        y=utils.to_numpy(tensor),
+        **kwargs,
+    ).show()
+
+
+def scatter(x, y, xaxis="", yaxis="", caxis="", **kwargs):
+    x = utils.to_numpy(x)
+    y = utils.to_numpy(y)
+    px.scatter(
+        y=y,
+        x=x,
+        labels={"x": xaxis, "y": yaxis, "color": caxis},
+        **kwargs,
+    ).show()
